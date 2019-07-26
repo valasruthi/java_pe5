f4ba6735d8326ef2ee510757adc302c657cdff39
valasruthi <valasruthi4@mail.com>
2019-07-26 11:30:24 +0530

    modifird setsorter

--- src/main/java/com/stackroute/pe5/SetSorter.java
+++ src/main/java/com/stackroute/pe5/SetSorter.java
@@ -1,20 +1,18 @@
 package com.stackroute.pe5;
 import java.util.*;
 public class SetSorter {
-  public Map methodForShiftingValues(Map<String, String> map) {
-    if(map.isEmpty())//checking for empty map
-      return null;
-    Iterator<Map.Entry<String,String>> iterator=map.entrySet().iterator();
-    int count=0;
-    String copy="";
-    while (iterator.hasNext()) {
-      Map.Entry<String, String> map1 = iterator.next();
-      if (count %2== 0) { //copying the value of next map value to present map value
-        copy = map1.getValue();
-        map.put(map1.getKey()," ");
-      }else
-        map.put(map1.getKey(),map1.setValue(copy));
+    public List<String> sortedList(String[] inputString) {
+//creation of arraylist
+      ArrayList<String> newArrayList = new ArrayList<>();
+      for (String str : inputString) {
+        newArrayList.add(str);
+      }
+      //creation of treeset and it will automatically sort the given input
+      Set treeSet = new TreeSet(newArrayList);
+      //storing sorted set into list
+      List list = new ArrayList(treeSet);
+      System.out.println(list);
+      return list;
     }
-    return map;
   }
 }
--- src/test/java/com/stackroute/pe5/SetSorterTest.java
+++ src/test/java/com/stackroute/pe5/SetSorterTest.java
@@ -1,45 +1,56 @@
 package com.stackroute.pe5;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import static org.junit.Assert.*;
-public class ValueShifterTest {
-  private SetSorter setSorter;
-  @Before//creating intance of valueshifter class
-  public void setUp(){
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+public class SortedListTest
+{
+  SetSorter setSorter;
+  @Before
+  public  void setup(){
+    //arrange
+    System.out.println("Inside Before");
     setSorter=new SetSorter();
   }
   @After
-  public void tearDown(){
+  public void tearDown()
+  {
+    System.out.println("Inside After");
     setSorter=null;
   }
-  @Test//test method for checking if the shifting of value is working or not
-  public void givenMethodWillCheckForValueShiftingOfValues(){
-    //input map
-    Map<String,String> map=new HashMap<>();
-    map.put("val1","java");
-    //act
-    Map map1=setSorter.methodForShiftingValues(map);
-    //assert
-    assertEquals(map1,map);
-  }
   @Test
-  public void givenMethodWillCheckForNullInputValue(){
-    //null map
-    Map<String,String> map=new HashMap<>();
+  //this testcase checks for given input should return sortedlist
+  public void givenStringShouldReturnSortedList()
+  {
+    String[] strings={"Harry","Olive","Alice","Bluto", "Eugene"};
     //act
-    Map map1=setSorter.methodForShiftingValues(map);
+    List actualResult=setSorter.sortedList(strings);
+    List list=new ArrayList();
+    list.add("Alice");
+    list.add("Bluto");
+    list.add("Eugene");
+    list.add("Harry");
+    list.add("Olive");
     //assert
-    assertNull(map1);
+    assertEquals(list,actualResult);
+  }
+  //this testcase checks for given input should not return sortedlist
+  @Test
+  public void givenStringShouldNotReturnSortedList()
+  {
+    String[] strings={"Harry","Olive","Alice","Bluto", "Eugene"};
+    //act
+    List actualResult=setSorter.sortedList(strings);
+    List expectedResult=new ArrayList();
+    expectedResult.add("Harry");
+    expectedResult.add("Olive");
+    expectedResult.add("Alice");
+    expectedResult.add("Bluto");
+    expectedResult.add("Eugene");
+    //assert
+    assertNotEquals(expectedResult,actualResult);
   }
 }
